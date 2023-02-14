31e7e0ede50e81b6189ce8ac8bf0d9d4be564d47
Jhenil Parihar <jhenilparihar@gmail.com>
2023-02-14 16:13:13 +0530

    added create view

--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 # Create your views here.
@@ -16,3 +17,9 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
